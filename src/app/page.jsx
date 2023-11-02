a4dfdaa56622535589d7e48377947b2dd84da301
ernazar1111 <ernazar2303@gmail.com>
2023-11-02 17:33:07 +0300

    3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,6 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <h1 className={styles.h1}>Real Estate</h1>
+    <h1 className={styles.h1}>Real Estate-3</h1>
   )
 }
